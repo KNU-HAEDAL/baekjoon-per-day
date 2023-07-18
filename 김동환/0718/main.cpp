de53db734ce6478cfa2e0b33ef29c39970f4ce22
gidskql6671 <gidskql6671@gmail.com>
2023-07-18 14:46:52 +0900

    형식 지정

---
+++ 김동환/0718/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+using namespace std;
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+
+  string a, b;
+  cin >> a >> b;
+
+  cout << (a.size() < b.size() ? "no" : "go") << endl;
+
+  return 0;
+}
