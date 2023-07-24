d2211f61bc0d02f94ce07a539d5ba4818908c76c
ajin <aj5210@naver.com>
2023-07-25 00:12:04 +0900

    udate code

--- 김아진/0724/2503_숫자야구.py
+++ 김아진/0724/2503_숫자야구.py
@@ -3,36 +3,31 @@ input = sys.stdin.readline
 
 n = int(input())
 
-answer = 0
-num = [0]*1000
+num_list = []
+for i in range(1,1000):
+    num = str(i)
+    if ('0' not in num) and (len(set(num)) == 3):
+        num_list.append(num)
+temp = []
 
 for _ in range(n):
     q, s, b = map(int, input().split())
-    q_num = list(map(int, str(q)))
-
-    for i in range(123, 1000):
-        a_num = list(map(int, str(i)))
+    q_num = str(q)
+    for num in num_list:
         strike = 0
         ball = 0
 
-        if a_num[0] == a_num[1] or a_num[0] == a_num[2] or a_num[1] == a_num[2]:
-            continue
-        if not all([a_num[0], a_num[1], a_num[2]]): # all도 있음
-            continue
-        
         for j in range(3):
             for k in range(3):
-                if a_num[j] == q_num[k]:
+                if q_num[j] == num[k]:
                     if j == k:
                         strike += 1
                     else:
                         ball += 1
-        
+
         if strike == s and ball == b:
-            num[i] += 1
+            temp.append(num)
+    num_list = temp[ : ]
+    temp.clear()
 
-for i in range(111, 1000):
-    if n == num[i]:
-        answer += 1
-
-print(answer)
+print(len(num_list))
