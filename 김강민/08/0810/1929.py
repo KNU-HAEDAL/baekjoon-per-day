bbb0fb547c7e6523e249a6d6042b5edbbf2c99d7
Dobbymin <79429482+Dobbymin@users.noreply.github.com>
2023-08-12 01:43:38 +0900

    8월 10일 소수 구하기

---
+++ 김강민/08/0810/1929.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+
+for i in range(M, N + 1):
+    if i == 1:
+        continue
+    for x in range(2, int(i ** 0.5) + 1):
+        if i % x == 0:
+            break
+    else:
+        print(i)
