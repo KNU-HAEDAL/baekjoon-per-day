2e2e48f17a2fd4d21a917abaff7fd898aea28577
이채은 <137969226+chaennlee@users.noreply.github.com>
2023-08-24 15:36:03 +0900

    Update baekjoon_14853_failversion.py

--- 이채은/8월/0824?/baekjoon_14853_failversion.py
+++ 이채은/8월/0824?/baekjoon_14853_failversion.py
@@ -1,3 +1,4 @@
 fail은 무슨.. 코드도 못 짰지만 머리가 터질 것 같다        
 
-file:///C:/Users/USER/Desktop/KakaoTalk_20230824_152712099.jpg 
+![KakaoTalk_20230824_152712099](https://github.com/KNU-HAEDAL/baekjoon-per-day/assets/137969226/c8f99f02-a458-4149-abc2-85e8aa1bfd9f)
+
